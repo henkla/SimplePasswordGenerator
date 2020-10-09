80a892ee4bd9a4a32781c94d525767a451fb88f1
henkla <lthlarsson@gmail.com>
2020-10-09 23:59:01 +0200

    pushing example project

---
+++ SimplePasswordGenerator.Example/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace SimplePasswordGenerator.Example
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // you'll need the actual generator
+            var generator = new Generator();
+
+            var helpText = $"Press any key to generate a new password (ESC to exit).";
+            Console.WriteLine(helpText + "\n");
+
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+                try
+                {
+                    Console.Clear();
+
+                    // let's generate a password
+                    // - it will remove all '@' chars since we provided it in the filter (which is optional)
+                    var myPassword = generator.Generate(passwordLength: 32, 
+                                                         Casing.Mixed,
+                                                         useSpecials: true,
+                                                         useNumerics: true,
+                                                         filter: "@");
+
+                    Console.WriteLine("The password is:\n");
+                    Console.WriteLine("--- BEGINNING OF PASSWORD ---");
+                    Console.WriteLine(myPassword);
+                    Console.WriteLine("--- END OF PASSWORD ---");
+                }
+                catch (GeneratorException e)
+                {
+                    PrintExceptionMessage(e);
+                }
+                finally
+                {
+                    Console.WriteLine("\n" + helpText);
+                }
+            }
+        }
+
+        private static void PrintExceptionMessage(GeneratorException e)
+        {
+            Console.WriteLine($"{e.GetType().Name}: {e.Message}");
+        }
+    }
+}
